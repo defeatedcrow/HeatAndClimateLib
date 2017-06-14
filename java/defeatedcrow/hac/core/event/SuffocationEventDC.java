ee76ebb166fb50c99b3e0c32bd2c71e815701374
defeatedcrow <defeatedcrow@gmail.com>
2017-06-14 23:09:20 +0900

    add suffocation mode
    - add: 地底で酸欠になるハードモードを追加
    - fix: BlockSet、ItemSetのhashcodeがworld間で一致しない不具合を修正

--- java/defeatedcrow/hac/core/event/SuffocationEventDC.java
+++ java/defeatedcrow/hac/core/event/SuffocationEventDC.java
@@ -6,6 +6,8 @@ import defeatedcrow.hac.api.damage.CamouflageInsideMaterialEvent;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityTameable;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -17,10 +19,11 @@ public class SuffocationEventDC {
 		Entity target = event.getEntity();
 		Material mat = event.material;
 		if (target != null && target.isEntityAlive() && mat == Material.WATER) {
+			boolean isTarget = target instanceof EntityPlayer || target instanceof EntityTameable;
 			double d0 = target.posY + target.getEyeHeight();
 			BlockPos pos = new BlockPos(target.posX, d0, target.posZ);
 			IBlockState state = target.worldObj.getBlockState(pos);
-			if (ClimateAPI.calculator.getAirflow(target.worldObj, pos) == DCAirflow.TIGHT) {
+			if (isTarget && ClimateAPI.calculator.getAirflow(target.worldObj, pos) == DCAirflow.TIGHT) {
 				event.setResult(Result.ALLOW);
 			}
 		}
