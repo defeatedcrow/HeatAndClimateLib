d57df9a9630f9d3f49c86024e020b22a5995ded2
defeatedcrow <defeatedcrow@gmail.com>
2018-07-08 01:26:30 +0900

    key config fix
    - change: Seasonにlang適用
    - fix: キーコンフィグが動作していない

--- java/defeatedcrow/hac/api/climate/EnumSeason.java
+++ java/defeatedcrow/hac/api/climate/EnumSeason.java
@@ -1,20 +1,27 @@
 package defeatedcrow.hac.api.climate;
 
+import net.minecraft.client.resources.I18n;
 import net.minecraft.item.EnumDyeColor;
 
 public enum EnumSeason {
 
-	SPRING(EnumDyeColor.PINK, 0),
-	SUMMER(EnumDyeColor.GREEN, 1),
-	AUTUMN(EnumDyeColor.ORANGE, 2),
-	WINTER(EnumDyeColor.BLUE, 3);
+	SPRING(EnumDyeColor.PINK, 0, "dcs.tip.spring"),
+	SUMMER(EnumDyeColor.LIME, 1, "dcs.tip.summer"),
+	AUTUMN(EnumDyeColor.ORANGE, 2, "dcs.tip.autumn"),
+	WINTER(EnumDyeColor.BLUE, 3, "dcs.tip.winter");
 
 	public final EnumDyeColor color;
 	public final int id;
+	public final String name;
 
-	private EnumSeason(EnumDyeColor c, int i) {
+	private EnumSeason(EnumDyeColor c, int i, String n) {
 		color = c;
 		id = i;
+		name = n;
+	}
+
+	public String getName() {
+		return I18n.format(name);
 	}
 
 }
